35cf1bcfb5cedae1952ad03aa84f3671128efb39
Bush <ulugbekbeshimov169@gmail.com>
2024-11-03 23:55:31 +0500

    task bar added

--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,13 +1,25 @@
 import org.example.enums.LogStatus;
 import org.example.person.employee.Employee;
+import org.example.person.employee.Waiter;
+import org.example.person.services.ChefService;
+import org.example.person.services.ManagerService;
+import org.example.person.services.ReceptionistService;
+import org.example.person.services.WaiterService;
+import org.example.restaurant.menu.objects.Meal;
+import org.example.restaurant.menu.objects.Menu;
+import org.example.restaurant.menu.objects.MenuSection;
+import org.example.restaurant.order.objects.Table;
 
 import java.io.*;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class TestMain{
 
     static String[] data = new String[20];
+    static Menu menu = null;
+    static Table table = null;
 
     public static void main(String[] args){
         //Creating a file called data
@@ -20,6 +32,10 @@ public class TestMain{
         LogStatus logStatus = LogStatus.LOGGED_OUT;
 
         //login
+        String login = logInOut(logStatus);
+
+        //doing tasks
+        chooseTask(login);
         logInOut(logStatus);
     }
 
@@ -38,7 +54,7 @@ public class TestMain{
     }
 
     //Logging in and logging out
-    public static void logInOut(LogStatus logStatus){
+    public static String logInOut(LogStatus logStatus){
         // Checking if user is logged in or not. If yes, then log out.
         // But if not, search for login and if it is valid, log in
         if(logStatus.equals(LogStatus.LOGGED_OUT)){
@@ -46,14 +62,16 @@ public class TestMain{
             System.out.println("Write your login : ");
             String login = scanner.next();
 
-            for(int i=0;i<20;i++){
+            for(int i = 0; i < 20; i++){
                 if(Objects.equals(login, data[i])){
                     logStatus = LogStatus.LOGGED_IN;
                     break;
                 }
             }
+            return login;
         }else{
             logStatus = LogStatus.LOGGED_OUT;
+            return "Logged out successfully";
         }
     }
 
@@ -74,4 +92,53 @@ public class TestMain{
         return data;
     }
 
+    //switching
+    public static void chooseTask(String login){
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        switch(login){
+            case "Manager":
+                ManagerService managerService = new ManagerService();
+                System.out.println("Choose what you want to do : ");
+                System.out.println("1. Add user : ");
+                managerService.addEmployee();
+                break;
+            case "Chef":
+                ChefService chefService = new ChefService();
+                System.out.println("Taking order...");
+                chefService.takeOrder(menu);
+            case "Waiter":
+                WaiterService waiterService = new WaiterService();
+                System.out.println("Choose what you want to do : ");
+                System.out.println("1.Create order\n2.Choose section\n3.Choose meal");
+                i = scanner.nextInt();
+                switch (i){
+                    case 1:
+                        waiterService.createOrder(menu,table);
+                        break;
+                    case 2:
+                        waiterService.chooseSection(menu);
+                        break;
+                    case 3:
+                        Meal meal = new Meal("pilau");
+                        List<Meal> meals = java.util.List.of();
+                        waiterService.chooseMeal(new MenuSection("Title","Descriiption",meals));
+                        break;
+                    default:
+                        System.out.println("Wrong input!!!");
+                }
+            case "Receptionist":
+                ReceptionistService receptionistService = new ReceptionistService();
+                receptionistService.createTableWithReservation();
+                break;
+            default:
+                System.out.println("Wrong input, or wrong statement");
+                break;
+        }
+        System.out.println("Type 1 to retry and anything else to log out");
+        i = scanner.nextInt();
+        if(i == 1){
+            chooseTask(login);
+        }
+    }
 }
---
+++ src/main/java/TestMain1.java
@@ -0,0 +1,14 @@
+import org.example.person.employee.Receptionist;
+import org.example.person.employee.Waiter;
+import org.example.restaurant.menu.objects.Meal;
+
+//create one object from all classes
+//User inputs are : Name, email, phone
+//System operations: search for name and if exists log in, if not, print "account not exists"
+//
+public class TestMain1 {
+    public static void main(String[] args){
+        //Create object of all classes
+
+    }
+}
